e8764c0a318ba42e455d0a04a68298648a14ec8e
Luis Dominguez <luis.dominguez.1208.ld@gmail.com>
2019-10-25 20:26:30 -0500

    eliminacion del proyecto final y actualizacion por archivos bases del curso

--- Examen_Version_php/js/index.js
+++ Examen_Version_php/js/index.js
@@ -5,22 +5,22 @@ $(function(){
 
 class Login {
   constructor() {
-    this.submitEvent();
+    this.submitEvent()
   }
 
   submitEvent(){
     $('form').submit((event)=>{
-      event.preventDefault();
-      this.sendForm();
-    });
+      event.preventDefault()
+      this.sendForm()
+    })
   }
 
   sendForm(){
     let form_data = new FormData();
-    form_data.append('username', $('#user').val());
-    form_data.append('password', $('#password').val());
+    form_data.append('username', $('#user').val())
+    form_data.append('password', $('#password').val())
     $.ajax({
-      url: 'server/check_login.php',
+      url: '../server/check_login.php',
       dataType: "json",
       cache: false,
       processData: false,
@@ -28,18 +28,15 @@ class Login {
       data: form_data,
       type: 'POST',
       success: function(php_response){
-        if (php_response.msg == "OK") window.location.href = 'main.html';
-        else alert(php_response.msg);
+        if (php_response.msg == "OK") {
+          window.location.href = 'main.html';
+        }else {
+          alert(php_response.msg);
+        }
       },
-      error: function(xhr, status, error){
-        
-        console.error("Status: " + status);
-        console.error("Error: " + error );          
-        console.error("ResponseText: " + xhr.responseText);
-
-        alert("Error en la comunicación con el servidor.");
-        
+      error: function(){
+        alert("error en la comunicación con el servidor");
       }
-    });
+    })
   }
 }
